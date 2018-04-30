fbfa5b8bb29ef8689f30a1c8d4772a0a5a834ff7
irof <irof@hogedriven.net>
2018-04-30 22:23:55 +0900

    Amazonリンクをアイコンにした

--- src/popup.js
+++ src/popup.js
@@ -12,12 +12,12 @@ document.getElementById("show").onclick = () => {
     chrome.storage.local.get("isbns", result => {
         result.isbns.forEach(isbn => {
             const li = document.createElement("li");
-            li.appendChild(document.createTextNode(isbn));
+            li.appendChild(document.createTextNode(isbn + " "));
 
             const link = document.createElement("a");
             link.href = "https://www.amazon.co.jp/gp/search?field-isbn=" + isbn;
             link.target = "_blank";
-            link.appendChild(document.createTextNode("Amazonで開いてみる"));
+            link.className = "fab fa-amazon";
             li.appendChild(link);
 
             ul.appendChild(li);
