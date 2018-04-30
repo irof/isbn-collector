510f127185c8960ae3a2b992a1eb186abfe37c3d
irof <irof@hogedriven.net>
2018-04-30 22:00:18 +0900

    日本のISBNだけ対象

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,7 +1,29 @@
+function isISBN10(value) {
+    if (value.startsWith("4")) {
+        // とりあえず日本だけ対象
+    }
+    return false;
+}
+function isISBN13(value) {
+    // とりあえず日本だけ対象
+    if (value.startsWith("9784")) {
+        return true;
+    }
+    return false;
+}
+
 function collectIsbn(text) {
-    const isbns = text.match(/\d{10,}/g)
-        .filter(candidate => candidate.length == 10 || candidate.length == 13);
-    return isbns;
+    const isbns = new Set();
+
+    for (const candidate of text.match(/\d{9,}[\dX]/g)) {
+        if (candidate.length == 10) {
+            if (isISBN10(candidate)) isbns.add(candidate);
+        }
+        if (candidate.length == 13) {
+            if (isISBN13(candidate)) isbns.add(candidate);
+        }
+    }
+    return Array.from(isbns);
 }
 
 const htmlText = document.documentElement.innerHTML;
